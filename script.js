cf0094717f1e37eff301e30b4a171bc0e7b99838
adamilnicki <adamilnicki00@gmail.com>
2021-09-15 20:09:00 +0200

    Mobile version, working JS button

---
+++ script.js
@@ -0,0 +1,40 @@
+let shareBtn = document.querySelectorAll(".btn");
+let shareDisplay = document.querySelector(".card-socials-container");
+let cardAuthor = document.querySelector(".card-author");
+let showSocials = false;
+
+function toggleSocials(){
+  let showSocials = false;
+  if(!showSocials){
+    shareDisplay.classList.remove("hidden");
+    cardAuthor.classList.add("hidden");
+
+    showSocials = true;
+  }else{
+    shareDisplay.classList.add("hidden");
+    cardAuthor.classList.remove("hidden");
+
+    showSocials = false;
+  }
+}
+
+shareBtn.forEach(function(item){
+  item.addEventListener("click",function(event){
+    event.preventDefault();
+  if(!showSocials){
+    shareDisplay.classList.remove("hidden");
+    cardAuthor.classList.add("hidden");
+
+    showSocials = true;
+  }else{
+    shareDisplay.classList.add("hidden");
+    cardAuthor.classList.remove("hidden");
+
+    showSocials = false;
+  }
+  });
+});
+
+
+// hide last child
+//cardAuthor.lastElementChild.classList.add("hidden");
